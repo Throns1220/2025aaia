bcb0c438afe030eed2a91151be2283344e092e90
Throns1220 <bruce20121220@gmail.com>
2025-03-14 11:58:19 +0800

    week04

---
+++ week04/week04-1.cpp
@@ -0,0 +1,13 @@
+// week04-1.cpp  C++ セ 璶糶︹
+// LeetCode 2529. Maximum Count of Positive Ineger and Negative Integer
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos=0, neg=0;
+        for( int i=0; i<nums.size();i++) {
+            if(nums[i]>0) pos++; // タ计++
+            if(nums[i]<0) neg++; // 璽计++
+        }
+        if(pos>neg) return pos; // タ计ゑ耕 癳
+        else return neg; // ぃ礛碞璽计ゑ耕 癳
+    }
---
+++ week04/week04-2.cpp
@@ -0,0 +1,10 @@
+// week04-2.cpp
+# include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++) {
+        if(i%2==0) printf("%d ",i);
+    }
+}
