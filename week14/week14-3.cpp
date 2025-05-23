5faf7aaaed3262c5300e16a46a3af990bee4a400
Throns1220 <bruce20121220@gmail.com>
2025-05-23 12:05:47 +0800

    week14

---
+++ week14/week14-3.cpp
@@ -0,0 +1,16 @@
+/// week14-3.cpp
+/// UVA11063: B2-Sequence
+/// Not OK, ONLY Part 1: Input Part 2: Output
+#include <stdio.h>
+int main()
+{
+	int a[100];
+	int n,t = 1;
+	while( scanf("%d",&n) == 1){; /// Part 1
+		for(int i=0;i<n;i++){ /// Part 1
+			scanf("%d", &a[i]);} /// Part 1
+		/// Part 2: Output
+		printf("Case #%d: It is a B2-Sequence.\n\n",t);
+		t++;
+	}
+}
